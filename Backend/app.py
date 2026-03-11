5730b5ca7328a537d6ade4845374a4d363b05c1f
XiaoWang1212 <danny20041212@gmail.com>
2026-03-11 11:29:23 +0800

    delete meaningless static code

--- Backend/app.py
+++ Backend/app.py
@@ -38,29 +38,6 @@ def create_app():
     app.register_blueprint(map_bp, url_prefix='/api/maps')
     app.register_blueprint(chat_bp, url_prefix='/api/chat')
 
-    # 添加靜態文件路由
-    @app.route('/css/<path:filename>')
-    def serve_css(filename):
-        return send_from_directory(os.path.join(ui_path, 'css'), filename)
-    
-    @app.route('/js/<path:filename>')
-    def serve_js(filename):
-        return send_from_directory(os.path.join(ui_path, 'js'), filename)
-    
-    @app.route('/assets/<path:filename>')
-    def serve_assets(filename):
-        return send_from_directory(os.path.join(ui_path, 'assets'), filename)
-
-    @app.route('/map')
-    def map_page():
-        return render_template('index.html')
-    
-    @app.route('/api/google-key')
-    def get_key():
-        return jsonify({
-        "key": app.config.get("GOOGLE_MAPS_API_KEY")
-    })
-
     @app.route('/')
     def index():
         return jsonify({
